83736a6fa28779173bdd57d0fdf8f4e8fb8f8de8
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2024-11-28 19:57:11 -0800

    userCont

--- routes/api.php
+++ routes/api.php
@@ -94,3 +94,5 @@ Route::delete('store-transfer-details/{id}', [StoreTransferDetailController::cla
 
 //get the user by the firebase ID mGgQzYeCj2NcX0K4AGUaov4BQhq2
 Route::get('fbUser/{FBID}', [UserController::class, 'getUserByFirebaseID']);
+
+Route::put('/users/{userId}', [RoleController::class, 'assignRoleToUser']);
